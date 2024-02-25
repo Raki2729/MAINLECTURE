144289c11f83fb06f92009d25df2499fd8c410d6
sorial <sorial2018@gmail.com>
2024-02-25 15:00:55 -0800

    Add routes and startup.js

---
+++ https-web-service/routes/startup.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const startup = express.Router()
+startup.get('/',(req,res)=>{
+    res.send('It is Working!!')
+})
+
+startup.get('/alive',(req,res)=>{
+    res.send('HTTPS-Web-Service is Alive')
+})
+// commomjs format
+module.exports = startup
