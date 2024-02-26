0bd30434464280d704ca00891a6799feaf01182e
sorial <sorial2018@gmail.com>
2024-02-25 21:27:48 -0800

    Add Assignment2 directory

--- Nodejs Basics/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const server = http.createServer((request,response) =>{
-    response.statusCode =200
-    response.setHeader('Content-Type','text/plain')
-    response.end("Hello from sfbu")
-})
-server.listen(8080,'localhost',() =>{
-    console.log("My Server is running!!")
-})
